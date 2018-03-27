754120c28fa513753fbdf89e3d347a36b4a7e43e
Lymke <mickael.rolo@gmail.com>
2018-03-28 00:19:57 +0200

    Config masquée

---
+++ config.dist/config.js
@@ -0,0 +1,9 @@
+module.exports =
+{
+    "minHourWaitingEvent" : 5,  // Temps minimum avant declanchement event en heure
+    "maxHourWaitingEvent" : 48, // Temps maximum avant declanchement event en heure
+    "firstEvent" : 1, // Temps avant le premier evenement en heure (fixe)
+    "channelID" : "**************", // id du salon ou sera diffuse le contenu
+    "prefix" : "*************",
+    "secretKey" : "******************"
+}
