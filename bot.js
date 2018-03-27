bb307852b588af12880eff9127542bb64c281b6e
defaultUser <you@example.com>
2018-03-27 13:04:50 +0200

    Add ; in end of lines

--- bot.js
+++ bot.js
@@ -7,23 +7,23 @@ var ChannelID = "231151508585054208" // id du salon ou sera diffuse le contenu
 var prefix = "!"
 
 function Joueur (id, pv) {
-	this.id = id
-	this.pv = pv
+	this.id = id;
+	this.pv = pv;
 	//this.time = Date.now() + firstEvent * 3600 * 1000
-	this.time = Date.now() + 5000
-	this.isWaitingEvent = true
+	this.time = Date.now() + 5000;
+	this.isWaitingEvent = true;
 
 	this.computeTimeNextEvent = function()
 	{		
-		var hour = Math.floor(Math.random()*maxHourWaitingEvent + minHourWaitingEvent)
-		this.time = Date.now() + hour * 3600 * 1000
-		console.log("Date actuelle en secondes: " + Date.now() / 1000)
-		console.log("Date prochaine en secondes: " + this.time / 1000)
+		var hour = Math.floor(Math.random()*maxHourWaitingEvent + minHourWaitingEvent);
+		this.time = Date.now() + hour * 3600 * 1000;
+		console.log("Date actuelle en secondes: " + Date.now() / 1000);
+		console.log("Date prochaine en secondes: " + this.time / 1000);
 	}
 }
 
 //Initialisation
-var tabJoueurs = new Array()
+var tabJoueurs = new Array();
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
@@ -33,10 +33,10 @@ client.on('ready', () => {
 
 client.on('message', msg => {
 	
-	var channel = client.channels.find("id", ChannelID)
+	var channel = client.channels.find("id", ChannelID);
 	if (channel == undefined)
 	{
-		console.log ("Erreur : salon " + ChannelID + "introuvable")
+		console.log ("Erreur : salon " + ChannelID + "introuvable");
 		return
 	}
 
@@ -44,29 +44,29 @@ client.on('message', msg => {
 	if (msg.content === prefix + 'decollage') {
 		// TODO : verifier que le joueur n'existe pas deja
 		channel.send(" Un decollage vient d'avoir lieu, celui de " + msg.author.username + " ! Parti explorer les fins fond de l'univers, va t'il aller au bout de son periple ?");
-		var j = new Joueur (msg.author.id, 10)
-		tabJoueurs.push(j)
+		var j = new Joueur (msg.author.id, 10);
+		tabJoueurs.push(j);
 	}
 	// TEST
 	if (msg.content === prefix + 'testl') {
 		// TODO : verifier que le joueur n'existe pas deja
-		channel.send("encodage : Oui " + msg.author.username + " je fonctionne super bien !");
-		var j = new Joueur (msg.author.id, 10)
-		tabJoueurs.push(j)
+		channel.send("Oui " + msg.author.username + " je fonctionne super bien !");
+		var j = new Joueur (msg.author.id, 10);
+		tabJoueurs.push(j);
 	}
 	
 	var joueur = tabJoueurs.find(function(element)
 	{
-		return element.id == msg.author.id
+		return element.id == msg.author.id;
 	});
 	
 	// Toute cette section concerne un joueur
 	if (joueur)
 	{
 		// Verifier si la personne a une nouvelle aventure
-		if (Date.now() >= joueur.time && joueur.isWaitingEvent)
+		if (Date.now() >= joueur.time && joueur.isWaitingEvent);
 		{
-			console.log("Et la, l'aventure commencera")
+			console.log("Et la, l'aventure commencera");
 		}		
 	}
 });
